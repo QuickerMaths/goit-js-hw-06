96b867234129484a189d6024dc3fe5cf22a05862
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-04-17 21:00:41 +0200

    commit

--- js/task-10.js
+++ js/task-10.js
@@ -5,8 +5,26 @@ function getRandomHexColor() {
 }
 
 const boxesEl = document.getElementById("boxes");
-const createBtn = document.querySelector("data-create");
-const destroyBtn = document.querySelector("data-destroy");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
 
-function createBoxes(amount) {}
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + 10 * i}px`;
+    box.style.height = `${30 + 10 * i}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesEl.appendChild(box);
+  }
+}
 
+createBtn.addEventListener("click", () => {
+  const amount = document.getElementById("input").value;
+  createBoxes(amount);
+});
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
+
+destroyBtn.addEventListener("click", destroyBoxes);
